99216127754c5f299e929e567844401acdcd1430
ELITE4 <blastcraft@mail.ru>
2021-09-22 14:23:09 +0200

    update 22.09
    * update base for latest csgo update

--- base/sdk/entity.h
+++ base/sdk/entity.h
@@ -577,13 +577,13 @@ public:
 	unsigned int PhysicsSolidMaskForEntity()
 	{
 		// @xref: "func_breakable", "func_breakable_surf"
-		return MEM::CallVFunc<unsigned int>(this, 151);
+		return MEM::CallVFunc<unsigned int>(this, 152);
 	}
 
 	bool IsPlayer()
 	{
 		// @xref: "effects/nightvision"
-		return MEM::CallVFunc<bool>(this, 157);
+		return MEM::CallVFunc<bool>(this, 158);
 	}
 
 	[[nodiscard]] Vector GetEyePosition(bool bShouldCorrect = true)
@@ -591,7 +591,7 @@ public:
 		Vector vecPosition = { };
 
 		// get eye position
-		MEM::CallVFunc<void, Vector&>(this, 168, vecPosition);
+		MEM::CallVFunc<void, Vector&>(this, 169, vecPosition);
 
 		// correct this like it do weapon_shootpos
 		// @ida weapon_shootpos: 55 8B EC 56 8B 75 08 57 8B F9 56 8B 07 FF 90
@@ -609,29 +609,29 @@ public:
 
 	void SetSequence(int iSequence)
 	{
-		MEM::CallVFunc<void>(this, 218, iSequence);
+		MEM::CallVFunc<void>(this, 219, iSequence);
 	}
 
 	void StudioFrameAdvance()
 	{
-		MEM::CallVFunc<void>(this, 219);
+		MEM::CallVFunc<void>(this, 220);
 	}
 
 	void UpdateClientSideAnimations()
 	{
 		// @xref: "UpdateClientSideAnimations"
 		// @ida updateclientsideanimations: 55 8B EC 51 56 8B F1 80 BE ? ? ? ? 00 74 ? 8B 06 FF
-		MEM::CallVFunc<void>(this, 223);
+		MEM::CallVFunc<void>(this, 224);
 	}
 
 	void PreThink()
 	{
-		MEM::CallVFunc<void>(this, 317);
+		MEM::CallVFunc<void>(this, 318);
 	}
 
 	void UpdateCollisionBounds()
 	{
-		MEM::CallVFunc<void>(this, 339);
+		MEM::CallVFunc<void>(this, 340);
 	}
 
 	bool PhysicsRunThink(int nThinkMethod)
@@ -661,53 +661,66 @@ public:
 class CCSWeaponData
 {
 public:
-	std::byte	pad0[0x14];				// 0x0000
-	int			iMaxClip1;				// 0x0014
-	int			iMaxClip2;				// 0x0018
-	int			iDefaultClip1;			// 0x001C
-	int			iDefaultClip2;			// 0x0020
-	int			iPrimaryMaxReserveAmmo; // 0x0024
-	int			iSecondaryMaxReserveAmmo; // 0x0028
-	char*		szWorldModel;			// 0x002C
-	char*		szViewModel;			// 0x0030
-	char*		szDroppedModel;			// 0x0034
-	std::byte	pad1[0x50];				// 0x0038
-	char*		szHudName;				// 0x0088
-	char*		szWeaponName;			// 0x008C
-	std::byte	pad2[0x2];				// 0x0090
-	bool		bIsMeleeWeapon;			// 0x0092
-	std::byte	pad3[0x9];				// 0x0093
-	float		flWeaponWeight;			// 0x009C
-	std::byte	pad4[0x28];				// 0x00A0
-	int			nWeaponType;			// 0x00C8
-	std::byte	pad5[0x4];				// 0x00CC
-	int			iWeaponPrice;			// 0x00D0
-	int			iKillAward;				// 0x00D4
-	std::byte	pad6[0x4];				// 0x00D8
-	float		flCycleTime;			// 0x00DC
-	float		flCycleTimeAlt;			// 0x00E0
-	std::byte	pad7[0x8];				// 0x00E4
-	bool		bFullAuto;				// 0x00EC
-	std::byte	pad8[0x3];				// 0x00ED
-	int			iDamage;				// 0x00F0
-	float		flArmorRatio;			// 0x00F4
-	int			iBullets;				// 0x00F8
-	float		flPenetration;			// 0x00FC
-	std::byte	pad9[0x8];				// 0x0100
-	float		flRange;				// 0x0108
-	float		flRangeModifier;		// 0x010C
-	float		flThrowVelocity;		// 0x0110
-	std::byte	pad10[0xC];				// 0x0114
-	bool		bHasSilencer;			// 0x0120
-	std::byte	pad11[0xB];				// 0x0121
-	char*		szBulletType;			// 0x012C
-	float		flMaxSpeed;				// 0x0130
-	float		flMaxSpeedAlt;			// 0x0134
-	float		flSpread;				// 0x0138
-	float		flSpreadAlt;			// 0x013C
-	std::byte	pad12[0x44];			// 0x0180
-	int			iRecoilSeed;			// 0x0184
-	std::byte	pad13[0x20];			// 0x0188
+	std::byte pad0[0x14];			// 0x0000
+	int iMaxClip1;					// 0x0014
+	int iMaxClip2;					// 0x0018
+	int iDefaultClip1;				// 0x001C
+	int iDefaultClip2;				// 0x0020
+	int iPrimaryMaxReserveAmmo;		// 0x0024
+	int iSecondaryMaxReserveAmmo;	// 0x0028
+	const char* szWorldModel;		// 0x002C
+	const char* szViewModel;		// 0x0030
+	const char* szDroppedModel;		// 0x0034
+	std::byte pad1[0x50];			// 0x0038
+	const char* szHudName;			// 0x0088
+	const char* szWeaponName;		// 0x008C
+	std::byte pad2[0x2];			// 0x0090
+	bool bIsMeleeWeapon;			// 0x0092
+	std::byte pad3[0x9];			// 0x0093
+	float flWeaponWeight;			// 0x009C
+	std::byte pad4[0x4];			// 0x00A0
+	int iSlot;						// 0x00A4
+	int iPosition;					// 0x00A8
+	std::byte pad5[0x1C];			// 0x00AC
+	int nWeaponType;				// 0x00C8
+	std::byte pad6[0x4];			// 0x00CC
+	int iWeaponPrice;				// 0x00D0
+	int iKillAward;					// 0x00D4
+	const char* szAnimationPrefix;	// 0x00D8
+	float flCycleTime;				// 0x00DC
+	float flCycleTimeAlt;			// 0x00E0
+	std::byte pad8[0x8];			// 0x00E4
+	bool bFullAuto;					// 0x00EC
+	std::byte pad9[0x3];			// 0x00ED
+	int iDamage;					// 0x00F0
+	float flArmorRatio;				// 0x00F4
+	int iBullets;					// 0x00F8
+	float flPenetration;			// 0x00FC
+	std::byte pad10[0xC];			// 0x0100
+	float flRange;					// 0x010C
+	float flRangeModifier;			// 0x0110
+	float flThrowVelocity;			// 0x0114
+	std::byte pad11[0xC];			// 0x0118
+	bool bHasSilencer;				// 0x0124
+	std::byte pad12[0xF];			// 0x0125
+	float flMaxSpeed[2];			// 0x0134
+	std::byte pad13[0x4];			// 0x013C
+	float flSpread[2];				// 0x0140
+	float flInaccuracyCrouch[2];	// 0x0148
+	float flInaccuracyStand[2];		// 0x0150
+	std::byte pad14[0x8];			// 0x0158
+	float flInaccuracyJump[2];		// 0x0160
+	float flInaccuracyLand[2];		// 0x0168
+	float flInaccuracyLadder[2];	// 0x0170
+	float flInaccuracyFire[2];		// 0x0178
+	float flInaccuracyMove[2];		// 0x0180
+	float flInaccuracyReload;		// 0x0188
+	int iRecoilSeed;				// 0x018C
+	float flRecoilAngle[2];			// 0x0190
+	float flRecoilAngleVariance[2];	// 0x0198
+	float flRecoilMagnitude[2];		// 0x01A0
+	float flRecoilMagnitudeVariance[2]; // 0x01A8
+	int iSpreadSeed;				// 0x01B0
 
 	bool IsGun()
 	{
@@ -774,22 +787,17 @@ public:
 
 	bool IsWeapon()
 	{
-		return MEM::CallVFunc<bool>(this, 165);
+		return MEM::CallVFunc<bool>(this, 166);
 	}
 
 	[[nodiscard]] float GetSpread()
 	{
-		return MEM::CallVFunc<float>(this, 452);
+		return MEM::CallVFunc<float>(this, 453);
 	}
 
 	[[nodiscard]] float GetInaccuracy()
 	{
-		return MEM::CallVFunc<float>(this, 482);
-	}
-
-	void UpdateAccuracyPenalty()
-	{
-		MEM::CallVFunc<void>(this, 483);
+		return MEM::CallVFunc<float>(this, 483);
 	}
 };
 
@@ -915,13 +923,13 @@ public:
 
 	void SendViewModelMatchingSequence(int nSequence)
 	{
-		MEM::CallVFunc<void>(this, 246, nSequence);
+		MEM::CallVFunc<void>(this, 247, nSequence);
 	}
 
 	void SetWeaponModel(const char* szFileName, CBaseCombatWeapon* pWeapon)
 	{
 		// @ida setweaponmodel: 57 8B F9 8B 97 ? ? ? ? 83 FA FF 74 6A
-		MEM::CallVFunc<void>(this, 247, szFileName, pWeapon);
+		MEM::CallVFunc<void>(this, 248, szFileName, pWeapon);
 	}
 };
 
