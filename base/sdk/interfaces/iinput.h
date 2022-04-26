10be87fb95a018cb0ca0a8ad0f1577469298e009
qo0 <blastcraft@mail.ru>
2022-04-26 13:13:27 +0200

    update 26.04
    * correct 'IInput' class padding to match latest cs:go update (closes #189, fixes #188)

--- base/sdk/interfaces/iinput.h
+++ base/sdk/interfaces/iinput.h
@@ -7,17 +7,17 @@
 class IInput
 {
 public:
-	std::byte			pad0[0xC];				//0x00
-	bool				bTrackIRAvailable;		//0x0C
-	bool				bMouseInitialized;		//0x0D
-	bool				bMouseActive;			//0x0E
-	std::byte			pad1[0x9E];				//0x0F
-	bool				bCameraInThirdPerson;	//0xAD
-	std::byte			pad2[0x2];				//0xAE
-	Vector				vecCameraOffset;		//0xB0
-	std::byte			pad3[0x38];				//0xBC
-	CUserCmd*			pCommands;				//0xF4
-	CVerifiedUserCmd*	pVerifiedCommands;		//0xF8
+	std::byte			pad0[0xC];				// 0x00
+	bool				bTrackIRAvailable;		// 0x0C
+	bool				bMouseInitialized;		// 0x0D
+	bool				bMouseActive;			// 0x0E
+	std::byte			pad1[0x9A];				// 0x0F
+	bool				bCameraInThirdPerson;	// 0xA9
+	std::byte			pad2[0x2];				// 0xAA
+	Vector				vecCameraOffset;		// 0xAC
+	std::byte			pad3[0x38];				// 0xB8
+	CUserCmd*			pCommands;				// 0xF0
+	CVerifiedUserCmd*	pVerifiedCommands;		// 0xF4
 
 	[[nodiscard]] CUserCmd* GetUserCmd(const int nSequenceNumber) const
 	{
@@ -29,4 +29,4 @@ public:
 		return &pVerifiedCommands[nSequenceNumber % MULTIPLAYER_BACKUP];
 	}
 };
-static_assert(sizeof(IInput) == 0xFC);
+static_assert(sizeof(IInput) == 0xF8);
