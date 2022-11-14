a30b55cdeecb0d3d04ab964faa9ba702184482ad
InterwebXplorer <InterwebXplorer@gmail.com>
2022-11-15 12:07:03 +1300

    Update 15.11
    Updated nlohmann/json to latest version + added filter for json library to solution

--- dependencies/json/detail/meta/identity_tag.hpp
+++ dependencies/json/detail/meta/identity_tag.hpp
@@ -1,4 +1,4 @@
-//     __ _____ _____ _____
+ //     __ _____ _____ _____
 //  __|  |   __|     |   | |  JSON for Modern C++
 // |  |  |__   |  |  | | | |  version 3.11.2
 // |_____|_____|_____|_|___|  https://github.com/nlohmann/json
@@ -14,11 +14,8 @@ NLOHMANN_JSON_NAMESPACE_BEGIN
 namespace detail
 {
 
-template<typename ...Ts> struct make_void
-{
-    using type = void;
-};
-template<typename ...Ts> using void_t = typename make_void<Ts...>::type;
+// dispatching helper struct
+template <class T> struct identity_tag {};
 
 }  // namespace detail
 NLOHMANN_JSON_NAMESPACE_END
--- dependencies/readme.txt
+++ dependencies/readme.txt
@@ -1,6 +1,6 @@
 dependencies versions:
 
 - imgui: 1.75 (modified)
-- nlohman json: 3.7.3
+- nlohman json: 3.11.2
 - freetype: 2.10.2
 - minhook: 1.3.3
